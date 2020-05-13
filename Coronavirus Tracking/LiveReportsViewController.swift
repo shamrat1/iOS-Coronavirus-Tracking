ec5b7612ae21956d200e4201d627e1feb453d518
shamrat1 <yshamrat@gmail.com>
2020-05-13 21:21:35 +0600

    tab view with assets added. Live reports page cell design

---
+++ Coronavirus Tracking/LiveReportsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LiveReportsViewController.swift
+//  Coronavirus Tracking
+//
+//  Created by Yasin Shamrat on 13/5/20.
+//  Copyright © 2020 Yasin Shamrat. All rights reserved.
+//
+
+import UIKit
+
+class LiveReportsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
