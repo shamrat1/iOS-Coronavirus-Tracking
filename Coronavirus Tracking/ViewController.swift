07ab988e7c15ff10096ad04e961825cbf4c75b67
shamrat1 <yshamrat@gmail.com>
2020-05-13 19:47:10 +0600

    Initial Commit

---
+++ Coronavirus Tracking/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Coronavirus Tracking
+//
+//  Created by Yasin Shamrat on 13/5/20.
+//  Copyright © 2020 Yasin Shamrat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
